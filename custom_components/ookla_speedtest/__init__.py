32ace9992b0ad440a30f6af0d082c243efd08c36
soulripper13 <55677941+soulripper13@users.noreply.github.com>
2025-04-29 18:02:02 +0800

    Create __init__.py

---
+++ custom_components/ookla_speedtest/__init__.py
@@ -0,0 +1,18 @@
+import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = [Platform.SENSOR]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Ookla Speedtest from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)

46712fd033e622cbf7dceffaacf9bfb0b6663b57
soulripper13 <55677941+soulripper13@users.noreply.github.com>
2025-04-30 00:01:38 +0800

    Update const.py

--- custom_components/ookla_speedtest/const.py
+++ custom_components/ookla_speedtest/const.py
@@ -4,5 +4,5 @@ DOMAIN = "ookla_speedtest"
 CONF_SERVER_ID = "server_id"
 CONF_MANUAL = "manual"
 CONF_SCAN_INTERVAL = "scan_interval"
-DEFAULT_SCAN_INTERVAL = 60  # minutes
+DEFAULT_SCAN_INTERVAL = 1440  # minutes (24 hours)
 SERVICE_RUN_SPEEDTEST = "run_speedtest"
